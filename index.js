b03bf1206f4d19862d93082e71817fa4590d3d84
nyctophiliacme <pransh7575@gmail.com>
2018-06-02 21:13:55 +0530

    Example app complete

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
-// app.get('/', function(req, res){
-// 	res.send('<h1>Hello World</h1>');
-// });
+var io = require('socket.io')(http);
 
 http.listen(3007, function(){
 	console.log("Listening on 3007");
@@ -11,4 +8,17 @@ http.listen(3007, function(){
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '\\index.html');
+});
+
+io.on('connection', function(socket){
+	console.log("A user has connected");
+
+	socket.on('chat-message', function(msg){
+		console.log("Message --> " + msg);
+		io.emit('chat-message', msg);
+	});
+
+	socket.on('disconnect', function(){
+		console.log("User disconnected");
+	});
 });
